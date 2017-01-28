d88baead6b4eba6a2422d39368f6bec6dca12a40
Albin "albinek" Sadowski <albinek@albinek.com>
2017-01-28 15:28:20 +0100

    Remove default logging

--- updater_for_macos.sh
+++ updater_for_macos.sh
@@ -1,41 +1,26 @@
 #!/usr/bin/env bash
 
 
-SCRIPT_DIRECTORY_PATH=$(dirname `which $0`)
-
-LOGS_FILE_NAME="logs.txt"
-LOGS_FILE_PATH=$SCRIPT_DIRECTORY_PATH/$LOGS_FILE_NAME
-
-
 START_DATE=$(date '%H:%M:%S | +%d.%m.%Y')
-echo "--- Updater for macOS started at: $START_DATE ---" > $LOGS_FILE_PATH
+echo "--- Updater for macOS started at: $START_DATE ---"
 
+echo "- Updating \"macOS\" -"
+softwareupdate --install --all
 
-echo "- Updating \"macOS\" -" >> $LOGS_FILE_PATH
-softwareupdate --install --all >> $LOGS_FILE_PATH
+echo "- Updating \"brew\" -"
+brew update
+brew cleanup
+brew cask update
+brew cask cleanup
 
+echo "- Updating \"gem\" -"
+gem update --system
+gem update
+gem cleanup
 
-echo "- Updating \"brew\" -" >> $LOGS_FILE_PATH
-brew update >> $LOGS_FILE_PATH
-brew cleanup >> $LOGS_FILE_PATH
-brew cask update >> $LOGS_FILE_PATH
-brew cask cleanup >> $LOGS_FILE_PATH
-
-
-echo "- Updating \"gem\" -" >> $LOGS_FILE_PATH
-gem update --system >> $LOGS_FILE_PATH
-gem update >> $LOGS_FILE_PATH
-gem cleanup >> $LOGS_FILE_PATH
-
-
-echo "- Updating \"apm\" -" >> $LOGS_FILE_PATH
-apm update --no-confirm >> $LOGS_FILE_PATH
-apm clean >> $LOGS_FILE_PATH
-
-
-echo "- Updating \"pod\" -" >> $LOGS_FILE_PATH
-pod setup >> $LOGS_FILE_PATH
-
+echo "- Updating \"apm\" -"
+apm update --no-confirm
+apm clean
 
 END_DATE=$(date '%H:%M:%S | +%d.%m.%Y')
-echo "--- Updater for macOS finished at: $END_DATE ---" >> $LOGS_FILE_PATH
+echo "--- Updater for macOS finished at: $END_DATE ---"
