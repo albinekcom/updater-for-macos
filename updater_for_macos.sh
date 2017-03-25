e461a8002c4d967bd5490285df3a648ed8fbf98a
Albin "albinek" Sadowski <albinek@albinek.com>
2017-03-25 09:15:42 +0100

    Add new lines to make output easier to read

--- updater_for_macos.sh
+++ updater_for_macos.sh
@@ -4,23 +4,28 @@
 START_DATE=$(date +"%d.%m.%Y | %H:%M:%S")
 echo "--- Updater for macOS started at: $START_DATE ---"
 
+echo
 echo "- Updating \"macOS\" -"
 softwareupdate --install --all
 
+echo
 echo "- Updating \"brew\" -"
 brew update
 brew upgrade
 brew cleanup
 brew cask cleanup
 
+echo
 echo "- Updating \"gem\" -"
 gem update --system
 gem update
 gem cleanup
 
+echo
 echo "- Updating \"apm\" -"
 apm update --no-confirm
 apm clean
 
 END_DATE=$(date +"%d.%m.%Y | %H:%M:%S")
+echo
 echo "--- Updater for macOS finished at: $END_DATE ---"
